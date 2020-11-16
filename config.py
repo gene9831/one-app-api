2c5793bd70822bffbf95a99f9b1bb8e4bb6a701a
gene9831 <pacify.98@gmail.com>
2020-11-16 18:39:19 +0800

    update mongo uri

--- config.py
+++ config.py
@@ -8,4 +8,4 @@ class Config:
     PROJECT_DIR = os.path.dirname(os.path.realpath(__file__))
 
     # MongoDB
-    MONGO_URI = 'mongodb://one:movie@192.168.0.126:27017/one_movie'
+    MONGO_URI = 'mongodb://one:oneapp@127.0.0.1:27017/one_app'
